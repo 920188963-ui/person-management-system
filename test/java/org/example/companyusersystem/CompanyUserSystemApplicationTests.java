9c2cfc57202fbb050b1b86fb4a0063999ce819b2
920188963-ui <920188963@qq.com>
2026-06-14 19:21:48 +0900

    companyUserSystem
    这是测试类

---
+++ test/java/org/example/companyusersystem/CompanyUserSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.companyusersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyUserSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
